de33bafc1c4a75217542b549bb46af920ecb5275
MaliSharfer <malis@skyvar.co.il>
2023-12-07 15:30:22 +0200

    add variables

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable subscription_id {
+  type        = string
+}
+
+
+variable rg_name {
+  type        = string
+}
+
+variable st_name {
+  type        = string
+  default     = "sttfstatemk2"
+}
