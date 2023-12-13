123c00813f4a4e27eddb3afca8ae4b1c54e70e31
MaliSharfer <malis@skyvar.co.il>
2023-12-13 13:07:27 +0200

    try11

--- main.tf
+++ main.tf
@@ -28,30 +28,45 @@ data "azurerm_storage_account" "vnet_storage_account"{
 
 
 resource "azurerm_service_plan" "service_plan" {
-  name                = each.value
+  name                = "app-subscriptions1"
   resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
   location            = data.azurerm_resource_group.vnet_resource_group.location
   os_type             = "Linux"
   sku_name            = "P1v2"
-  for_each = toset(var.app_service_plan_name)
+  # for_each = toset(var.app_service_plan_name)
 }
 
 resource "azurerm_linux_function_app" "linux_function_app" {
-  name                = var.function_app_name[count.index]
+  name                = "func-subscriptions1"
   resource_group_name = data.azurerm_storage_account.vnet_storage_account.resource_group_name
   location            = data.azurerm_storage_account.vnet_storage_account.location
 
   storage_account_name       = data.azurerm_storage_account.vnet_storage_account.name
   storage_account_access_key = data.azurerm_storage_account.vnet_storage_account.primary_access_key
-  service_plan_id            = azurerm_service_plan.service_plan[var.app_service_plan_name[count.index]].id
+  service_plan_id            = azurerm_service_plan.service_plan.id
 
-  app_settings = {for key, value in var.function_app_settings[count.index]: key =>value}
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "NUM_OF_MONTHS" = 1
+    "COST" = 100
+    "TABLE_SUBSCRIPTIONS_TO_DELETE" = "subscriptionsToDelete"
+    "TABLE_DELETED_SUBSCRIPTIONS" = "deletedSubscriptions"
+    "TABLE_SUBSCRIPTIONS_MANAGERS" = "subscriptionManagers"
+    "TABLE_EMAILS" = "emails"
+    "HTTP_TRIGGER_URL" = "https://function-send-email.azurewebsites.net/api/HttpTrigger1?code=_t5Z5vfMdHzcb2iHjbnv3sD-RQQ9V7uoqSJLFt6iXvBJAzFuMVp4hQ=="
+    "RECIPIENT_EMAIL" = "YeuditC@skyvar.co.il"
+    "SECRET" = "CONNECTION-STRING"
+    "KEYVAULT_NAME" = "kv-chaya-try"
+    "KEYVAULT_URI" = "https://kv-chaya-try.vault.azure.net"
+    "SHELIS_EMAIL" = "ChayaH@skyvar.co.il"
+    "TAG_NAME" = "essential"
+  }
   site_config {}
 
   identity {
     type = "SystemAssigned"
   }
 
-  count = length(azurerm_service_plan.service_plan)
+  # count = length(azurerm_service_plan.service_plan)
 
 }
