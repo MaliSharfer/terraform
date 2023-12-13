7a4b869daa0cf5cb39eb4ff606f765e7203d465b
MaliSharfer <malis@skyvar.co.il>
2023-12-13 15:16:38 +0200

    try19

--- main.tf
+++ main.tf
@@ -22,39 +22,69 @@ data "azurerm_resource_group" "vnet_resource_group" {
 }
 
 data "azurerm_storage_account" "vnet_storage_account"{
-  name ="myfirsttrail"
+  name =var.vnet_storage_account_name
   resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
 }
 
+resource "azurerm_network_security_group" "network_security_group" {
+  name                = var.nsg_name
+  location            = data.azurerm_resource_group.vnet_resource_group.location
+  resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "app-service-plan-newnew21"
-  location            = "East US"
-  resource_group_name = data.azurerm_storage_account.vnet_storage_account.resource_group_name
+  security_rule {
+    name                       = var.security_rule_name
+    priority                   = var.security_rule_priority
+    direction                  = var.security_rule_direction
+    access                     = var.security_rule_access
+    protocol                   = var.security_rule_protocol
+    source_port_range          = var.security_rule_source_port_range
+    destination_port_range     = var.security_rule_destination_port_range
+    source_address_prefix      = var.security_rule_source_address_prefix
+    destination_address_prefix = var.security_rule_destination_address_prefix
+  }
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = var.vnet_name
+  location            = data.azurerm_resource_group.vnet_resource_group.location
+  resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
+  address_space       = var.address_space
+  dns_servers         = var.dns_servers
+}
+
+resource "azurerm_subnet" "vnet_subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = data.azurerm_resource_group.vnet_resource_group.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = each.value
+  location            = data.azurerm_resource_group.vnet_resource_group.location
+  resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
   kind                = "Linux"
   reserved            = true
   sku {
     tier = "Dynamic"
     size = "Y1"
   }
+  for_each = toset(var.app_service_plan_name)
 }
 
-resource "azurerm_function_app" "example" {
-  name                      = "mali-function-newnew11"
+resource "azurerm_function_app" "function_app" {
+  name                      = var.function_app_name[count.index]
   location                  = data.azurerm_storage_account.vnet_storage_account.location
   resource_group_name       = data.azurerm_storage_account.vnet_storage_account.resource_group_name
-  app_service_plan_id       = azurerm_app_service_plan.example.id
+  app_service_plan_id       = azurerm_app_service_plan.app_service_plan[var.app_service_plan_name[count.index]].id
   storage_account_name      = data.azurerm_storage_account.vnet_storage_account.name
-  storage_account_access_key = data.azurerm_storage_account.vnet_storage_account.primary_access_key
+  storage_account_access_key =data.azurerm_storage_account.vnet_storage_account.primary_access_key
   os_type                   = "linux"
   version                   = "~4"
 
-  app_settings = {
-  "FUNCTIONS_WORKER_RUNTIME" = "python"
-  "AzureWebJobsStorage"      = data.azurerm_storage_account.vnet_storage_account.primary_connection_string
-  
-  }
-
+  app_settings = {for key, value in var.function_app_settings[count.index]: key =>value}
   site_config {
     linux_fx_version = "python|3.11"
   }
@@ -62,10 +92,39 @@ resource "azurerm_function_app" "example" {
   identity {
     type = "SystemAssigned"
   }
+  
+  count = length(azurerm_app_service_plan.app_service_plan)
+}
 
+resource "azurerm_logic_app_workflow" "logic_app_workflow" {
+  name                = each.value
+  location            = data.azurerm_resource_group.vnet_resource_group.location
+  resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
+  for_each            = toset(var.logic_app_workflow_name)
+}
+
+data "azurerm_client_config" "current_client" {}
+
+data "azurerm_key_vault" "key_vault" {
+  name = "kv-chaya-try"
+  resource_group_name = "rg-chaya-subscription-management"
 }
 
 
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_function_app.function_app.identity[0].principal_id 
+}
+
+resource "azurerm_role_assignment" "key_vault_access" {
+  scope                = data.azurerm_key_vault.key_vault.id 
+  role_definition_name = "Key Vault Administrator"  
+  principal_id         = azurerm_function_app.function_app.identity[0].principal_id
+}
 
 
 
--- variables.tf
+++ variables.tf
@@ -1,72 +1,150 @@
+# secret
 variable subscription_id {
   type        = string
 }
 
-
 variable rg_name {
   type        = string
+  default = "NetworkWatcherRG"
 }
 
-variable st_name {
+variable rg_location {
   type        = string
-  default     = "sttfstatemk3"
+  default = "West Europe"
+}
+
+variable nsg_name {
+  type        = string
+  default = "nsg-environment-automations"
+}
+
+variable security_rule_name {
+  type        = string
+  default = "test123"
+}
+
+variable security_rule_priority {
+  type        = number
+  default     = 1000
+}
+
+variable security_rule_direction {
+  type        = string
+  default     = "Outbound"
+}
+
+variable security_rule_access {
+  type        = string
+  default     = "Allow"
+}
+
+variable security_rule_protocol {
+  type        = string
+  default     = "*"
+}
+
+variable security_rule_source_port_range {
+  type        = string
+  default     = "*"
+}
+
+variable security_rule_destination_port_range {
+  type        = string
+  default     = "*"
+}
+
+variable security_rule_source_address_prefix {
+  type        = string
+  default     = "*"
+}
+
+variable security_rule_destination_address_prefix {
+  type        = string
+  default     = "*"
+}
+
+
+variable vnet_name {
+  type        = string
+  default = "vnet-environment-automations"
+}
+
+variable address_space {
+  type        = list
+  default = ["10.1.0.0/16"]
+}
+
+variable dns_servers {
+  type        = list
+  default = []
+}
+
+variable subnet_name {
+  type        = string
+  default = "snet-environment-automations"
+}
+
+variable subnet_address_prefix {
+  type        = list
+  default = ["10.1.1.0/24"]
+}
+
+variable vnet_storage_account_name {
+  type        = string
+  default =  "myfirsttrail"
 }
 
 variable app_service_plan_name{
   type = list(string)
-  default =  ["app-subscriptions1","app-storage-accounts1","app-emails1"]
+  default =  ["app-subscriptions","app-storage-accounts","app-emails"]
 }
 
 variable function_app_name {
   type        = list(string)
-  default =  ["func-subscriptions1","func-storageaccounts1","func-emails1"]
+  default =  ["func-subscriptions","func-storageaccounts","func-emails"]
 }
 
 variable function_app_settings {
   type        = list
-  default = [{
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "NUM_OF_MONTHS" = 1
-    "COST" = 100
-    "TABLE_SUBSCRIPTIONS_TO_DELETE" = "subscriptionsToDelete"
-    "TABLE_DELETED_SUBSCRIPTIONS" = "deletedSubscriptions"
-    "TABLE_SUBSCRIPTIONS_MANAGERS" = "subscriptionManagers"
-    "TABLE_EMAILS" = "emails"
-    "HTTP_TRIGGER_URL" = "https://function-send-email.azurewebsites.net/api/HttpTrigger1?code=_t5Z5vfMdHzcb2iHjbnv3sD-RQQ9V7uoqSJLFt6iXvBJAzFuMVp4hQ=="
-    "RECIPIENT_EMAIL" = "YeuditC@skyvar.co.il"
-    "SECRET" = "CONNECTION-STRING"
-    "KEYVAULT_NAME" = "kv-chaya-try"
-    "KEYVAULT_URI" = "https://kv-chaya-try.vault.azure.net"
-    "SHELIS_EMAIL" = "ChayaH@skyvar.co.il"
-    "TAG_NAME" = "essential"
-  },{
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "DESIRED_TIME_PERIOD_SINCE_LAST_RETRIEVAL_FOR_CHECK_LAST_FETCH" = 30
-    "DESIRED_TIME_PERIOD_SINCE_LAST_RETRIEVAL_FOR_CHECK_USED_CAPACITY" = 30
-    "TIME_INDEX_FOR_CHECK_LAST_FETCH"="days"
-    "TIME_INDEX_FOR_CHECK_USED_CAPACITY"="days"
-    "FREQ_AUTOMATION_TEST_TYPE"="weeks"
-    "FREQ_AUTOMATION_TEST_NUMBER"=1
-    "ALERTS_DOCUMENTATION"="alertsDocumentation"
-    "DOCUMENTATION_TABLE" ="documentation"
-    "DELETED_ACCOUNTS_TABLE"="deletedStorages"
-    "DOCUMENTATION_STORAGE_NAME"="myfirsttrail"
-    "WORKSPACE_ID" = "fa9e707a-28c1-4528-b7b2-54d03360d4c9"
-    "HTTP_TRIGGER_URL"="https://func-try-2.azurewebsites.net/api/HttpTrigger1?code=vqQyTSrot8Byr3-PUAWsHWWUBRImjzQp9DO_i8itYgKmAzFueI86Pg=="
-    "MAIN_MANAGER"="batyag@skyvar.co.il"
-    "ESSENTIAL_TAG"="essential"
-  },{
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "EMAIL_SENDER" = "clouddg99@gmail.com"
-    "SENDER_EMAIL_PASSWORD" = "sfipmkunckgmrshg"
-    "SMTP_HOST" = "smtp.gmail.com"
-    "SMTP_PORT" = 465
-    "SECRET" = "CONNECTION-STRING"
-    "KEYVAULT_NAME" = "kv-chaya-try"
-    "KEYVAULT_URI" = "https://kv-chaya-try.vault.azure.net"
-  }]
+}
+
+variable logic_app_workflow_name {
+  type        = list(string)
+  default =["logic-app-storage-management","logic-app-subscription-management"]
 }
 
 
+variable key_vault_name {
+  type        = string
+  default = "kv-manageautomations"
+}
 
+variable key_vault_sku_name {
+  type        = string
+  default     = "standard"
+}
 
+variable key_vault_certificate_permissions {
+  type        = list
+  default = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"]
+}
+
+variable key_vault_key_permissions {
+  type        = list
+  default = ["Create","Get"]
+}
+
+variable key_vault_secret_permissions {
+  type        = list
+  default = ["Get","Set","Delete","Purge","Recover"]
+}
+
+variable key_vault_storage_permissions {
+  type        = list
+  default =  ["Get", ]
+}
+
+variable key_vault_secret_name {
+  type        = string
+  default     = "CONNECTION-STRING"
+}
