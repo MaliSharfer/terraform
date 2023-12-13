98cfecaa2f5bf82955ab72de0443c904a4ab33f0
MaliSharfer <malis@skyvar.co.il>
2023-12-13 14:27:36 +0200

    try16

--- main.tf
+++ main.tf
@@ -27,46 +27,41 @@ data "azurerm_storage_account" "vnet_storage_account"{
 }
 
 
-resource "azurerm_service_plan" "service_plan" {
-  name                = "service_plan215"
-  resource_group_name = "NetworkWatcherRG"
-  location            = "West Europe"
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-#   for_each = toset(var.app_service_plan_name)
+resource "azurerm_app_service_plan" "example" {
+  name                = "app-service-plan-newnew21"
+  location            = "East US"
+  resource_group_name = data.azurerm_storage_account.myfirsttrail.resource_group_name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
 
-resource "azurerm_linux_function_app" "linux_function_app" {
-  name                = "functionapp558"
-  resource_group_name = "NetworkWatcherRG"
-  location            = "West Europe"
-
-  storage_account_name       = "myfirsttrail"
-  storage_account_access_key = "Lnpas6aKSxVwAicyeYf/B2J8KLlUny0dQFY8svrReSf35KWJ/emvSDz5s8N1mxdzukqddnqP1Ohn+AStosSBzA=="
-  service_plan_id            = azurerm_service_plan.service_plan.id
+resource "azurerm_function_app" "example" {
+  name                      = "mali-function-newnew11"
+  location                  = data.azurerm_storage_account.myfirsttrail.location
+  resource_group_name       = data.azurerm_storage_account.myfirsttrail.resource_group_name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_account_name      = data.azurerm_storage_account.myfirsttrail.name
+  storage_account_access_key = data.azurerm_storage_account.myfirsttrail.primary_access_key
+  os_type                   = "linux"
+  version                   = "~4"
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "NUM_OF_MONTHS" = 1
-    "COST" = 100
-    "TABLE_SUBSCRIPTIONS_TO_DELETE" = "subscriptionsToDelete"
-    "TABLE_DELETED_SUBSCRIPTIONS" = "deletedSubscriptions"
-    "TABLE_SUBSCRIPTIONS_MANAGERS" = "subscriptionManagers"
-    "TABLE_EMAILS" = "emails"
-    "HTTP_TRIGGER_URL" = "https://function-send-email.azurewebsites.net/api/HttpTrigger1?code=_t5Z5vfMdHzcb2iHjbnv3sD-RQQ9V7uoqSJLFt6iXvBJAzFuMVp4hQ=="
-    "RECIPIENT_EMAIL" = "YeuditC@skyvar.co.il"
-    "SECRET" = "CONNECTION-STRING"
-    "KEYVAULT_NAME" = "kv-chaya-try"
-    "KEYVAULT_URI" = "https://kv-chaya-try.vault.azure.net"
-    "SHELIS_EMAIL" = "ChayaH@skyvar.co.il"
-    "TAG_NAME" = "essential" 
+  "FUNCTIONS_WORKER_RUNTIME" = "python"
+  "AzureWebJobsStorage"      = data.azurerm_storage_account.myfirsttrail.primary_connection_string
+  
   }
-  site_config {}
+
+  site_config {
+    linux_fx_version = "python|3.11"
+  }
 
   identity {
     type = "SystemAssigned"
   }
-
 
 }
 
