aa4abf6f3aef83176d239d3a3a718e9101fa5d37
MaliSharfer <malis@skyvar.co.il>
2023-12-13 16:18:38 +0200

    try22

--- main.tf
+++ main.tf
@@ -113,64 +113,15 @@ data "azurerm_key_vault" "key_vault" {
 data "azurerm_subscription" "primary" {
 }
 
-resource "azurerm_role_assignment" "example" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Reader"
-  principal_id         = azurerm_function_app.function_app[0].identity[0].principal_id 
-}
-
-resource "azurerm_role_assignment" "key_vault_access" {
-  scope                = data.azurerm_key_vault.key_vault.id 
-  role_definition_name = "Key Vault Administrator"  
-  principal_id         = azurerm_function_app.function_app[0].identity[0].principal_id
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# resource "azurerm_linux_function_app" "linux_function_app" {
-#   name                = "func-subscriptions1"
-#   resource_group_name = data.azurerm_storage_account.vnet_storage_account.resource_group_name
-#   location            = data.azurerm_storage_account.vnet_storage_account.location
-
-#   storage_account_name       = data.azurerm_storage_account.vnet_storage_account.name
-#   storage_account_access_key = data.azurerm_storage_account.vnet_storage_account.primary_access_key
-#   service_plan_id            = azurerm_service_plan.service_plan.id
-
-#   app_settings = {
-#     "FUNCTIONS_WORKER_RUNTIME" = "python"
-#     "NUM_OF_MONTHS" = 1
-#     "COST" = 100
-#     "TABLE_SUBSCRIPTIONS_TO_DELETE" = "subscriptionsToDelete"
-#     "TABLE_DELETED_SUBSCRIPTIONS" = "deletedSubscriptions"
-#     "TABLE_SUBSCRIPTIONS_MANAGERS" = "subscriptionManagers"
-#     "TABLE_EMAILS" = "emails"
-#     "HTTP_TRIGGER_URL" = "https://function-send-email.azurewebsites.net/api/HttpTrigger1?code=_t5Z5vfMdHzcb2iHjbnv3sD-RQQ9V7uoqSJLFt6iXvBJAzFuMVp4hQ=="
-#     "RECIPIENT_EMAIL" = "YeuditC@skyvar.co.il"
-#     "SECRET" = "CONNECTION-STRING"
-#     "KEYVAULT_NAME" = "kv-chaya-try"
-#     "KEYVAULT_URI" = "https://kv-chaya-try.vault.azure.net"
-#     "SHELIS_EMAIL" = "ChayaH@skyvar.co.il"
-#     "TAG_NAME" = "essential"
-#   }
-#   site_config {}
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-
-#   # count = length(azurerm_service_plan.service_plan)
-
+# resource "azurerm_role_assignment" "subscription_access" {
+#   scope                = data.azurerm_subscription.primary.id
+#   role_definition_name = "Reader"
+#   principal_id         = azurerm_function_app.function_app[0].identity[0].principal_id 
 # }
+
+# resource "azurerm_role_assignment" "key_vault_access" {
+#   scope                = data.azurerm_key_vault.key_vault.id 
+#   role_definition_name = "Key Vault Administrator"  
+#   principal_id         = azurerm_function_app.function_app[0].identity[0].principal_id
+# }
+
