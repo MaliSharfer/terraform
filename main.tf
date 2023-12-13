8b1565c93dea6d23eb5aed39ea8b8bff40d68328
MaliSharfer <malis@skyvar.co.il>
2023-12-13 13:00:28 +0200

    try10

--- main.tf
+++ main.tf
@@ -17,7 +17,41 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "state_resource_group" {
+data "azurerm_resource_group" "vnet_resource_group" {
   name     = var.rg_name
-  location = "West Europe"
+}
+
+data "azurerm_storage_account" "vnet_storage_account"{
+  name ="myfirsttrail"
+  resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
+}
+
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = each.value
+  resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
+  location            = data.azurerm_resource_group.vnet_resource_group.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+  for_each = toset(var.app_service_plan_name)
+}
+
+resource "azurerm_linux_function_app" "linux_function_app" {
+  name                = var.function_app_name[count.index]
+  resource_group_name = data.azurerm_storage_account.vnet_storage_account.resource_group_name
+  location            = data.azurerm_storage_account.vnet_storage_account.location
+
+  storage_account_name       = data.azurerm_storage_account.vnet_storage_account.name
+  storage_account_access_key = data.azurerm_storage_account.vnet_storage_account.primary_access_key
+  service_plan_id            = azurerm_service_plan.service_plan[var.app_service_plan_name[count.index]].id
+
+  app_settings = {for key, value in var.function_app_settings[count.index]: key =>value}
+  site_config {}
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  count = length(azurerm_service_plan.service_plan)
+
 }
